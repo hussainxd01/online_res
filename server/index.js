d9cf616dff479ea8f6d9e9ceaee5fcd0a7692db9
Md Hussain <hussainxd01@gmail.com>
2025-02-25 13:39:31 +0530

    server initial commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import connectDB from "./libs/db.js";
+
+// Load environment variables
+dotenv.config();
+
+// Initialize Express app
+const app = express();
+
+// Middleware
+app.use(express.json()); // Body parser
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser()); // Parse cookies
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true })); // CORS setup
+app.use(morgan("dev")); // Logger
+
+// Database connection
+connectDB();
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
